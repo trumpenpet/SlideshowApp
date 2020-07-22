711b46bf7746e8d4b9028913035f9627a4ceab6e
trumpenpet <trumpenpet@gmail.com>
2020-07-23 07:22:21 +0900

    Initial Commit

---
+++ SlideshowApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp2
+//
+//  Created by 小島大舗 on 2020/07/23.
+//  Copyright © 2020 daisuke.kojima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
